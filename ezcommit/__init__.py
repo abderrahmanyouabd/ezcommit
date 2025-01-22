634028471a38385e3643c93f1000ad5ee6e3b8b3
Abderrahman Youabd <youabd50@gmail.com>
2025-01-23 00:50:55 +0100

    Bump version to 0.1.1

--- ezcommit/__init__.py
+++ ezcommit/__init__.py
@@ -2,6 +2,6 @@
 EffortZeroCommit - Automated Git Commit Message Generator
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Abderrahman Youabd"
 __author_email__ = "youabd50@gmail.com"
